632cdf96cd72f836ca8eb63bcc85709dd802e608
Edgarvital <edgar.tecnico@hotmail.com>
2023-01-25 20:31:22 -0300

    Arquivo JS onde o jquery é utilizado para consumir uma API que a partir do CEP retorna algumas informações a respeito do endereço, que são utilizadas para preencher os inputs do formulário

---
+++ js/cep.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#cep').on('change', function(){
+        var tamanhoDoCep = this.value.length;
+        if(tamanhoDoCep == 8) {
+            var cep = this.value;
+
+            fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    $('#estado').val(data.uf);
+                    $('#cidade').val(data.localidade);
+                    $('#bairro').val(data.bairro);
+                    $('#rua').val(data.logradouro);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+    });
+});
